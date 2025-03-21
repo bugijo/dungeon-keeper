d6a90385912b3aeec4566a841e3099c3ed89ea20
bugijo <gil.jonathan.pereira@gmail.com>
2025-03-21 18:43:13 -0300

    Adiciona __init__.py para o pacote de magias

---
+++ src/systems/magic/__init__.py
@@ -0,0 +1,27 @@
+from .spell import (
+    Spell,
+    SpellSchool,
+    SpellType,
+    CastType,
+    TargetType,
+    Element,
+    SpellEffect,
+    SpellRequirement
+)
+from .spellbook import SpellBook, SpellSlot
+from .elemental import ElementalSystem, ElementalInteraction
+
+__all__ = [
+    'Spell',
+    'SpellSchool',
+    'SpellType',
+    'CastType',
+    'TargetType',
+    'Element',
+    'SpellEffect',
+    'SpellRequirement',
+    'SpellBook',
+    'SpellSlot',
+    'ElementalSystem',
+    'ElementalInteraction'
+]
